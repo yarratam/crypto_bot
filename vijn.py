3e119095530a818973fb195d2388bb988e12fd99
alamofire <ivan.galinov28@gmail.com>
2022-03-13 00:10:50 +0300

    new buttons

--- vijn.py
+++ vijn.py
@@ -1,12 +1,22 @@
 from itertools import cycle
 
 alp = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+alp_upper = alp.upper()
+
+
+def crypt_char(arg):
+    a = alp if arg[0].islower() else alp_upper
+    return a[(a.index(arg[0])+a.index(arg[1])%33)%33]
+
+
+def decrypt_char(arg):
+    a = alp if arg[0].islower() else alp_upper
+    return a[a.index(arg[0])-a.index(arg[1])%33]
+
 
 def crypt_vijn(text, key):
-    f = lambda arg: alp[(alp.index(arg[0])+alp.index(arg[1])%33)%33]
-    return ''.join(map(f, zip(text, cycle(key))))
+    return ''.join(map(crypt_char, zip(text, cycle(key))))
 
 
 def decrypt_vijn(text, key):
-    f = lambda arg: alp[alp.index(arg[0])-alp.index(arg[1])%33]
-    return ''.join(map(f, zip(text, cycle(key))))
+    return ''.join(map(decrypt_char, zip(text, cycle(key))))
